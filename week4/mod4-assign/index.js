aae78a1e784b8dffd76f651108df4f0cad44664b
Charles A. Koenig <charleston.ak@gmail.com>
2021-11-11 04:03:44 -0800

    Added footer

---
+++ week4/mod4-assign/index.js
@@ -0,0 +1,80 @@
+// var x = "Hello World!";
+// var mess = "In Global";
+// console.log("Global Message: " + mess);
+//
+// var a = function () {
+//   // var mess = "Inside A ";
+//   console.log("A message: " + mess);
+//   function b() {
+//     console.log("B: " + mess );
+//   }
+// b();
+//   }
+//
+// a();
+// var x;
+// console.log(x);
+// if(x==undefined) {
+//   console.log("X is undefined");
+// }
+// x=5;
+// if (x=undefined) {
+//   console.log("X is undefined");
+// }
+// else {
+//   console.log("X has been undefined");
+// }
+
+// String concatinatin
+
+// var string = "Hello";
+// string += " World";
+// console.log(string + "!");
+
+// Math Operators
+
+// Regular Math Operators: +, -, *, /
+
+// console.log((5+5)/3);
+// console.log(undefined/5);
+
+// equality
+// var x = 4;
+// var y = 4;
+// if(x==y) {
+// console.log("x = 4 is equal y = 4");
+// }
+// x = "4";
+// if("x==y") {
+//   console.log("x='4' is equal to y='4' ");
+// }
+//
+
+// commmon language construct part1
+
+// best practice for the curly braces
+
+// function a() {
+// return {
+//    name: "Yarles"
+//  };
+// }
+// function b() {
+// return {
+//   name: "Yoenig"
+// };
+// }
+// console.log(a());
+// console.log(b());
+//
+// // var sum = 0;
+// // for(var i = 0; i<10; i++) {
+// //   console.log(i);
+// //   sum = sum + i ;
+// //   console.log("The sum of zero through nine is: " + sum);
+// }
+
+// object creation
+
+
+ // var company = new object();
